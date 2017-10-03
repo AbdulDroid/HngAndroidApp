6841adb3e4e3be5aeda211809f806e86138859df
Emem Brownson <brownjee001@gmail.com>
2017-10-03 18:05:23 +0100

    testhashkey

--- app/src/main/java/team_galaxy/hnginterns/hngmobileapp/ui/activities/MainActivity.java
+++ app/src/main/java/team_galaxy/hnginterns/hngmobileapp/ui/activities/MainActivity.java
@@ -1,7 +1,7 @@
 package team_galaxy.hnginterns.hngmobileapp.ui.activities;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import team_galaxy.hnginterns.hngmobileapp.R;
 
@@ -11,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
     }
+
 }
